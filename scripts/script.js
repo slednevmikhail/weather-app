83cd677ec030fe8b7cb15ab4447243eebb5a2376
yeb1ch <89690018+yeb1ch@users.noreply.github.com>
2022-11-20 02:04:55 +0500

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+
+const GetWeatherInfo = (event) => {
+    event.preventDefault()
+    const latitude = (form.querySelector('.request-input__latitude')).value.trim()
+    const longitude = (form.querySelector('.request-input__longitude')).value.trim()
+    if (IsValid(latitude, longitude)){
+        GetWeatherData(latitude, longitude).then((weatherData) => FormatData(weatherData, latitude, longitude))
+    }
+}
+
+const IsValid = (inputValues) => {
+    const firstValue = parseFloat(inputValues[0])
+    const secondValue = parseFloat(inputValues[1])
+    return typeof firstValue === 'number' && typeof secondValue === 'number'
+        && !isNaN(firstValue) && !isNaN(secondValue)
+        && -90 <= firstValue <= 90 && -180 <= secondValue <= 180;
+}
+
+const GetWeatherData = async (latitude, longitude) => {
+    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d1e4d1d53f86f2eae9c9de8aee8124ec&lang=ru&units=metric`)
+    const weatherInfo = await request.json()
+    return await weatherInfo
+}
+
+const FormatData = (weatherInfo, latitude, longitude) => {
+    const iconSrc = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`
+    const weatherSection = document.getElementById("weather-section")
+    const ajaxContainer = weatherSection.querySelector('.weather-section__container')
+    const div = document.createElement("div");
+    div.classList.add("weather-section_content");
+    const markup = `
+        <div class="weather-section__city-name">${weatherInfo.name}</div>
+        <div class="weather-section__temp">погода - ${weatherInfo.main.temp}<sup>°C</sup></div>
+        <div class="weather-section__windspeed">скорость ветра - ${weatherInfo.wind.speed} м/с</div>
+        <div class="weather-section__img">
+            <img src="iconSrc">
+        </div>
+      `;
+    div.innerHTML = markup;
+    ajaxContainer.appendChild(div);
+}
+
+const form = document.getElementById("request-input__form")
+form.addEventListener("submit", GetWeatherInfo)
