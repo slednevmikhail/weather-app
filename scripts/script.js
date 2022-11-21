8e1227ee9c9b391386ddb761214753976f2900eb
yeb1ch <89690018+yeb1ch@users.noreply.github.com>
2022-11-22 03:45:07 +0500

    n

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,31 @@
+let activeWidgetsAmount = 0
+let lastCreatedWidget;
 
-const GetWeatherInfo = (event) => {
+const ControlEvent = (event) => {
     event.preventDefault()
+    console.log(event.target.id)
+    if (event.target.id === "request-input__form"){
+        GetWeatherInfo()
+    }
+    else {
+        if (lastCreatedWidget !== undefined){
+            RemoveWidgets(lastCreatedWidget)
+        }
+        else {
+            alert("Ошибка! Виджетов нет")
+        }
+    }
+}
+const GetWeatherInfo = () => {
     const latitude = (form.querySelector('.request-input__latitude')).value.trim()
     const longitude = (form.querySelector('.request-input__longitude')).value.trim()
     if (IsValid(latitude, longitude)){
-        GetWeatherData(latitude, longitude).then((weatherData) => FormatData(weatherData, latitude, longitude))
+        GetWeatherData(latitude, longitude).then((weatherData) => AddWidget(weatherData, latitude, longitude))
     }
     else {
         alert('Ошибка! Неправильно введены координаты! Код ошибки:')
     }
 }
-
 const IsValid = (inputValues) => {
     const firstValue = Number(inputValues[0])
     const secondValue = Number(inputValues[1])
@@ -30,12 +45,12 @@ const GetWeatherData = async (latitude, longitude) => {
     }
 }
 
-const FormatData = (weatherInfo, latitude, longitude) => {
+const AddWidget = (weatherInfo, latitude, longitude) => {
     const iconSrc = `https://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png`
     const weatherSection = document.getElementById("weather-section")
     const ajaxContainer = weatherSection.querySelector('.weather-section__container')
     const div = document.createElement("div");
-    div.classList.add("weather-section_content");
+    div.classList.add(`weather-section_content-${activeWidgetsAmount}`);
     const markup = `
         <div class="weather-section__city-name">${weatherInfo.name}</div>
         <div class="weather-section__temp">погода : ${weatherInfo.main.temp}<sup>°C</sup></div>
@@ -46,7 +61,12 @@ const FormatData = (weatherInfo, latitude, longitude) => {
       `;
     div.innerHTML = markup;
     ajaxContainer.appendChild(div);
+    activeWidgetsAmount += 1;
+    lastCreatedWidget = div
 }
 
+const RemoveWidgets = (div) => {
+    div.classList.remove(`weather-section_content-${activeWidgetsAmount}`)
+}
 const form = document.getElementById("request-input__form")
-form.addEventListener("submit", GetWeatherInfo)
+form.addEventListener("submit", ControlEvent)
