f7a32e4d3f758c867ffbb28b6ca923482474b753
yeb1ch <89690018+yeb1ch@users.noreply.github.com>
2022-11-22 02:56:44 +0500

    n

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,9 @@ const GetWeatherInfo = (event) => {
     if (IsValid(latitude, longitude)){
         GetWeatherData(latitude, longitude).then((weatherData) => FormatData(weatherData, latitude, longitude))
     }
+    else {
+        alert('Введенные данные некорректны!')
+    }
 }
 
 const IsValid = (inputValues) => {
@@ -17,9 +20,14 @@ const IsValid = (inputValues) => {
 }
 
 const GetWeatherData = async (latitude, longitude) => {
-    const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d1e4d1d53f86f2eae9c9de8aee8124ec&lang=ru&units=metric`)
-    const weatherInfo = await request.json()
-    return await weatherInfo
+    try {
+        const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d1e4d1d53f86f2eae9c9de8aee8124ec&lang=ru&units=metric`)
+        const weatherInfo = await request.json()
+        return await weatherInfo
+    }
+    catch (exceptionVar){
+        alert(`Произошла ошибка,код ошибки: ${exceptionVar}`)
+    }
 }
 
 const FormatData = (weatherInfo, latitude, longitude) => {
@@ -30,10 +38,10 @@ const FormatData = (weatherInfo, latitude, longitude) => {
     div.classList.add("weather-section_content");
     const markup = `
         <div class="weather-section__city-name">${weatherInfo.name}</div>
-        <div class="weather-section__temp">погода - ${weatherInfo.main.temp}<sup>°C</sup></div>
+        <div class="weather-section__temp">погода : ${weatherInfo.main.temp}<sup>°C</sup></div>
         <div class="weather-section__windspeed">скорость ветра - ${weatherInfo.wind.speed} м/с</div>
         <div class="weather-section__img">
-            <img src="iconSrc">
+            <img src="${iconSrc}">
         </div>
       `;
     div.innerHTML = markup;
